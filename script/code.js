77cb743d98031b37016ca0bf6a8022fea3082fac
GustavoBOY <gustavoboy1702@gmail.com>
2023-04-23 14:45:04 -0300

    Botao subir

--- script/code.js
+++ script/code.js
@@ -5,9 +5,25 @@ if (window.location.hash) {
     var scrollWithoutHash = window.location.href.replace(window.location.hash, '');
     window.history.replaceState('', document.title, scrollWithoutHash);
 }
-  
-    
-
 window.addEventListener("beforeunload", function () {
 window.scrollTo(0, 0);
 });
+
+
+
+var btnTopo = document.getElementById("botao-subir");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btnTopo.style.display = "block";
+  } else {
+    btnTopo.style.display = "none";
+  }
+}
+
+function subirFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
