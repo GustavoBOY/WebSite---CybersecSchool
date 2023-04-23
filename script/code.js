f1d5d6a62d31ceb74e2815f00b8c80a38ba79ee3
GustavoBOY <gustavoboy1702@gmail.com>
2023-04-23 14:31:29 -0300

    Ajuste javascript

--- script/code.js
+++ script/code.js
@@ -1,7 +1,10 @@
 window.onload = function () {
     window.scrollTo(0, 0);
-  };
+};
+if (window.location.hash) {
+window.history.replaceState(null, null, window.location.href.split("#")[0]);
+}
 
 window.addEventListener("beforeunload", function () {
-    window.scrollTo(0, 0);
-  });
+window.scrollTo(0, 0);
+});
